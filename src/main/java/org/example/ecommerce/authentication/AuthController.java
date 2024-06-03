b10d5f8e6642cd9101c184e1c7f230ab4da1f711
Madhav <madhav.moole99@gmail.com>
2024-06-03 14:16:33 +0530

    Setup user registration and made changes to Models

--- src/main/java/org/example/ecommerce/authentication/AuthController.java
+++ src/main/java/org/example/ecommerce/authentication/AuthController.java
@@ -1,5 +1,35 @@
 package org.example.ecommerce.authentication;
 
+import org.example.ecommerce.authentication.model.RegistrationDTO;
+import org.example.ecommerce.authentication.service.AuthService;
+import org.example.ecommerce.authentication.service.IAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    private IAuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity registerUser(@RequestBody RegistrationDTO registrationDTO) {
+        try {
+            authService.createUser(registrationDTO);
+            return ResponseEntity.ok().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
 
 }
