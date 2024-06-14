bbd152ae4401d00bb8fe693e78371e94c4af6a5c
Madhav <madhav.moole99@gmail.com>
2024-06-14 22:48:09 +0530

    Code Cleanup

--- src/main/java/org/example/ecommerce/utils/ValidatationService.java
+++ src/main/java/org/example/ecommerce/utils/ValidatationService.java
@@ -2,7 +2,7 @@ package org.example.ecommerce.utils;
 
 import java.util.regex.Pattern;
 
-public class Validator {
+public class ValidatationService {
 
     public static boolean isValidEmail(String emailAddress) {
         String regexPattern = "^(?=.{1,64}@)[\\p{L}0-9_-]+(\\.[\\p{L}0-9_-]+)*@[^-][\\p{L}0-9-]+(\\.[\\p{L}0-9-]+)*(\\.[\\p{L}]{2,})$";
