03e0a3137b19d06c289cac8f9a85d524578dbad7
Madhav <madhav.moole99@gmail.com>
2024-06-22 13:08:33 +0530

    restricted endpoints by adding authorization

--- src/main/java/org/example/ecommerce/authentication/security/WebSecurityConfig.java
+++ src/main/java/org/example/ecommerce/authentication/security/WebSecurityConfig.java
@@ -5,16 +5,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 public class WebSecurityConfig {
+
+    private final JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable);
+        http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
         http.authorizeHttpRequests(auth->
-                auth.anyRequest().permitAll()
+                auth.requestMatchers("/product/v1").permitAll()
+                        .requestMatchers("/auth/register").permitAll()
+                        .requestMatchers("/auth/login").permitAll()
+                        .anyRequest().authenticated()
         );
         return http.build();
     }
