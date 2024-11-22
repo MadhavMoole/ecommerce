dd479039ba75c59e60781df4554c1860a1796709
Madhav <madhav.moole99@gmail.com>
2024-11-22 21:29:03 +0530

    feat: updated WebSocketManager

--- src/main/java/org/example/ecommerce/websockets/WebSocketConfig.java
+++ src/main/java/org/example/ecommerce/websockets/WebSocketConfig.java
@@ -1,5 +1,8 @@
 package org.example.ecommerce.websockets;
 
 public class WebSocketConfig {
-
+    public static WebSocketConfig getWebSocketConfig() {
+        return new WebSocketConfig();
+    }
 }
+0
